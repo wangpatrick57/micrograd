d8105bcd86080a5115ff03bb179579b0d22b890c
Patrick Wang <wang.patrick57@gmail.com>
2025-09-27 17:37:44 -0700

    tanh

--- engine.py
+++ engine.py
@@ -1,20 +1,24 @@
 from enum import Enum
 
+import numpy as np
+
 
 class Op(Enum):
     NONE = ""
     ADD = "+"
     MUL = "*"
+    TANH = "tanh"
 
 
 class Value:
     def __init__(self, data: float, _prev: list["Value"] = [], _op: Op = Op.NONE):
         self.data = data
+        self.grad: float | None = None
         self._prev = _prev
         self._op = _op
 
     def __repr__(self) -> str:
-        return f"Value(data={self.data}, _prev={self._prev}, _op={self._op})"
+        return f"Value(data={self.data})"
 
     def __add__(self, other: "Value") -> "Value":
         return Value(self.data + other.data, [self, other], Op.ADD)
@@ -22,9 +26,15 @@ class Value:
     def __mul__(self, other: "Value") -> "Value":
         return Value(self.data * other.data, [self, other], Op.MUL)
 
+    def tanh(self) -> "Value":
+        return Value(
+            (np.exp(self.data) - np.exp(-self.data))
+            / (np.exp(self.data) + np.exp(-self.data)),
+            [self],
+            Op.TANH,
+        )
+
 
 if __name__ == "__main__":
     a = Value(2.0)
-    b = Value(-3.0)
-    c = Value(10.0)
-    print(a * b + c)
+    print(a.tanh())
